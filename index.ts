e8ee491368c6bbb5be010fefe21e6f2c401e142a
nycbasic <samuel.poon@baruchmail.cuny.edu>
2021-01-13 21:22:17 -0500

    completed mini app with small use of typescript

---
+++ index.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}
+  `);
+};
+
+
+axios.get(url).then((res) => {
+  const { id, title, completed } = res.data as Todo;
+  logTodo(id, title, completed);
+});
+
+
